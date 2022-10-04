1b034ea2b1b750a1292398df405b9a589f551a20
Simon Kristiansen <simon@kristianzen.dk>
2022-10-04 16:55:07 +0200

    change names in elgamal, add pdf

--- ElGamal/Program.fs
+++ ElGamal/Program.fs
@@ -1,3 +1,5 @@
+module ElGamal
+
 open System.Numerics
 open System.Security.Cryptography
 
@@ -12,11 +14,11 @@ let c2 = m * BigInteger.ModPow(pk, r, p)
 let bruteforce g p pk =
     let rec aux (i: int) =
        match BigInteger.ModPow(g, i, p) = pk with
-        | true -> i
+        | true -> BigInteger i
         | false -> aux (i+1)
-    aux 0
-let bfpk = bruteforce g p pk // Eve now has Bob's private key, 66
-let grsk = BigInteger.ModPow(c1, bfpk, p)
+    aux 1
+let bfsk = bruteforce g p pk // Eve now has Bob's private key, 66
+let grsk = BigInteger.ModPow(c1, bfsk, p)
 let decrypted = c2 / grsk // Eve now has the decrypted message, 2000
 let malloryc2 = c2 * BigInteger 3 // Mallory doesn't have Bob's private key, but seeing that the message is simply an integer they can multiply it by 3 as 2000 * 3 = 6000
 let bobMalloryDecrypt = malloryc2 / grsk // Bob decrypting Mallory's message, 6000
